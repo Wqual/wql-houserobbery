fb48e59f1aa43d6fa412c4986e663ddc0f629d69
Wqual Development <130603719+Wqual@users.noreply.github.com>
2023-05-12 23:02:05 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,10 +5,12 @@ lua54 'yes'
 author 'Wqual#7949'
 descritpion 'House Robbery by Wqual'
 
-
 shared_script "@es_extended/imports.lua"
 shared_script '@ox_lib/init.lua'
 
+shared_scripts {
+    'shared/**.lua'
+}
 
 client_scripts {
     'client/**.lua'
