df05809eb9aceebfe52541230333f40a03c63ed5
Wqual Development <130603719+Wqual@users.noreply.github.com>
2023-05-12 23:01:10 +0200

    Add files via upload

---
+++ shared/config_house_robbery.lua
@@ -0,0 +1,25 @@
+WQL = {} -- Wqual® --
+
+WQL.EntrataCasa = vector3(-150.0051, 123.7391, 70.2254) -- Marker Entrata 
+
+WQL.TeleportEntrata = vector3(266.02584, -1007.537, -101.0087) -- Teletrasporto entrata 
+
+WQL.UscitaCasa = vector3(266.02584, -1007.537, -101.0087) -- Marker Uscita 
+
+WQL.TeleportUscita = vector3(-150.5392, 123.1947, 70.2253) -- Teletrasporto uscita 
+
+WQL.DepositoCasa = vector3(261.4666, -1002.6627, -99.0086) -- posizione deposito 1
+
+WQL.DepositoCasa2 = vector3(265.8155, -999.4681, -99.0086) -- posizione deposito 2
+
+WQL.DepositoCasa3 = vector3(257.0169, -995.6896, -99.0087) -- posizione deposito 3
+
+WQL.Traduzione = {
+
+    ["entra"] = "Forza serratura",
+    ["esci"] = "Esci dalla casa",
+    ["frigobar"] = "Interagisci col cassetto",
+    ["deposito1"] = "Interagisci con mobile",
+    ["deposito2"] = "Interagisci col cassetto"
+
+}
